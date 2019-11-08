c9a442a806660242a1c8a4a5a81f93fc829d3454
zhanjian <jian.zhan@okcoin.net>
2019-11-08 11:59:48 +0800

    add

---
+++ src/main/java/easy/PalindromeNumber_9.java
@@ -0,0 +1,43 @@
+package easy;
+
+/**
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ * 示例 1:
+ *
+ * 输入: 121
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 示例 3:
+ *
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ */
+public class PalindromeNumber_9 {
+
+  private static boolean isPalindrome(int x) {
+    if(x < 10 && x > -1){
+      return true;
+    }
+    if(x < 0){
+      return false;
+    }
+    int res = 0;
+    int temp = x;
+    while(temp > 0){
+      res = res * 10 + (temp % 10);
+      temp = temp / 10;
+    }
+    return res == x;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(isPalindrome(-10));
+  }
+
+}
