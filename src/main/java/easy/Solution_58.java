95ca89a27aca26689ed6dd7affc72954833ccc5a
zhanj1an <18246698331@163.com>
2019-12-17 12:08:10 +0800

    add 58

---
+++ src/main/java/easy/Solution_58.java
@@ -0,0 +1,23 @@
+package easy;
+
+/**
+ * 58. 最后一个单词的长度
+ * 给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+ * <p>
+ * 如果不存在最后一个单词，请返回 0 。
+ * <p>
+ * 说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: "Hello World"
+ * 输出: 5
+ */
+public class Solution_58 {
+  public int lengthOfLastWord(String s) {
+    String[] strings = s.split(" ");
+    if (strings.length == 0)
+      return 0;
+    return strings[strings.length - 1].length();
+  }
+}
